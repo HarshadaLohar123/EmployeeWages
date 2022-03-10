04984cac872e8d852d1b3d767530cbf79430b2a5
harshadalohar123 <harshalohar123@gmail.com>
2022-03-10 16:21:30 +0530

    ADDED:Employee PartTime

--- Uc1-Employee Present Or Absent/Uc1-Employee Present Or Absent/Class1.cs
+++ Uc1-Employee Present Or Absent/Uc1-Employee Present Or Absent/Class1.cs
@@ -8,42 +8,65 @@ namespace Uc1_Employee_Present_Or_Absent
 {
     public class Class1
     {
-        int IS_PRSENT = 1;
-        int IS_ABSENT = 0;
-        Random random = new Random();
+        Random obj = new Random();
+        int Is_Full_Time = 1;
+        int Is_Part_Time = 2;
+
+        int maxRateInMonth = 100;
+        int Emp_Rate_Per_Hr = 20;
+        int numOfWorkingDays = 20;
+        int totalWorkingDay = 20;
         int empHrs = 0;
         int empWage = 0;
-        int EMP_RATE_PER_HR=0;
+        
         public void Uc1Employee()
         {
-
-            int empCheck = random.Next(0, 2);
-
-            if (empCheck == IS_PRSENT)
+            int empCheck = obj.Next(0, 2);
+            if (empCheck == Is_Full_Time)
             {
-                Console.WriteLine("Employee Is Present");
-
+                Console.WriteLine("Employee is Present");
             }
             else
             {
-                Console.WriteLine("Employee Is Absent");
-
+                Console.WriteLine("Employee is Absent");
             }
         }
         public void Uc2_DailyWages()
         {
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRSENT)
+            int empCheck = obj.Next(0, 2);
+            if (empCheck == Is_Full_Time)
             {
                 empHrs = 8;
+                Console.WriteLine("He worked full day");
             }
             else
             {
                 empHrs = 0;
+                Console.WriteLine("He is Absent");
             }
-            empWage = empHrs * EMP_RATE_PER_HR;
-
-            Console.WriteLine("EmpWage : " + empWage);
+            empWage = empHrs * Emp_Rate_Per_Hr;
+            Console.WriteLine("Employee salary is= " + empWage);
+        }
+        public void Uc3_PartTime()
+        {
+            int randomCheck = obj.Next(0, 3);
+            if (randomCheck == Is_Full_Time)
+            {
+                empHrs = 8;
+                Console.WriteLine("He Worked Full Day");
+            }
+            else if (randomCheck == Is_Part_Time)
+            {
+                empHrs = 4;
+                Console.WriteLine("He Worked Half Day");
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("He Is Absent");
+            }
+            empWage = empHrs * Emp_Rate_Per_Hr;
+            Console.WriteLine("His Salary is= " + empWage);
 
         }
     }
--- Uc1-Employee Present Or Absent/Uc1-Employee Present Or Absent/Program.cs
+++ Uc1-Employee Present Or Absent/Uc1-Employee Present Or Absent/Program.cs
@@ -9,6 +9,7 @@ namespace Uc1_Employee_Present_Or_Absent
             Class1 obj = new Class1();
             obj.Uc1Employee();
             obj.Uc2_DailyWages();
+            obj.Uc3_PartTime();
         }
     }
 }
