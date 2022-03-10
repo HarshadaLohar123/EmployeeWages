f8757f6196b219cb4290a91b8f0f56dea6fb6a70
harshadalohar123 <harshalohar123@gmail.com>
2022-03-10 14:38:53 +0530

    Added:Employee Absent Or Present

---
+++ Uc1-Employee Present Or Absent/Uc1-Employee Present Or Absent/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc1_Employee_Present_Or_Absent
+{
+    public class Class1
+    {
+        public void Uc1Employee()
+        {
+            int IS_PRSENT = 1;
+            int IS_ABSENT = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_PRSENT)
+            {
+                Console.WriteLine("Employee Is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+
+            }
+        }
+
+    }
+}
---
+++ Uc1-Employee Present Or Absent/Uc1-Employee Present Or Absent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uc1_Employee_Present_Or_Absent
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.Uc1Employee();
+        }
+    }
+}
