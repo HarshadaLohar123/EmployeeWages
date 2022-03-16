c195996b91e72a3e5a9175d69e3398793d1bd4ad
harshadalohar123 <harshalohar123@gmail.com>
2022-03-16 13:04:40 +0530

    ADDED: Compute Employee Wage Using Class Method

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpWageUsingClass
+{
+    public class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            EmpWage empWage = new EmpWage();
+            empWage.Uc_7_EmpWageUsingClass();
+        }
+    }
+}
