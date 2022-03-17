b20473129a3dc40fe4c97e953c71a01e34576f6e
harshadalohar123 <harshalohar123@gmail.com>
2022-03-17 14:48:34 +0530

    ADDED : Multiple Company Using Interface

--- EmpWageBuilderArray.cs
+++ EmpWageBuilderArray.cs
@@ -60,34 +60,10 @@ namespace MultipleCompanyUsingInterface
             }
             return totalEmpHrs * companyEmpWage.empRatePerHour;
         }
-    }
-    public class CompanyEmpWage
-    {
-        public string company;
-        public int empRatePerHour;
-        public int numOfWorkingDays;
-        public int maxHoursPerMonth;
-        public int totalEmpWage;
-
-        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonth = maxHoursPerMonth;
-
-        }
-        public void setTotalEmpWage(int totalEmpWage)
-        {
-            this.totalEmpWage = totalEmpWage;
-        }
-        public string toString()
-        {
-            return "Total Emp Wage for Company : " + this.company + " is: " + this.totalEmpWage;
-        }
-    }
-
+    }      
 }
+
+
 
 
 
