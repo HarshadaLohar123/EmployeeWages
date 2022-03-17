e0a505448d05f03ac45d4ffe0edacad95ead8f71
harshadalohar123 <harshalohar123@gmail.com>
2022-03-17 13:13:43 +0530

    ADDED: Save The Total Wage For Each Company

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TotalWageForEachComapany
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage dMart = new EmpWage("DMart", 20, 2, 10);
+            EmpWage reliance = new EmpWage("Reliance", 10, 4, 20);
+            dMart.Uc_9_EmpTotalWageForEachCompany();
+            Console.WriteLine(dMart.toString());
+            reliance.Uc_9_EmpTotalWageForEachCompany();
+            Console.WriteLine(reliance.toString());
+
+        }
+    }
+}
