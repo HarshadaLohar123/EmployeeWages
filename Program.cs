10879ff8080c47ea63cb67f21500a0c5fadf8877
harshadalohar123 <harshalohar123@gmail.com>
2022-03-17 13:17:42 +0530

    ADDED: Employee Wage for Multiple Company using Interface

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MultipleCompanyUsingInterface
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           EmpWageBuilderArray empWage = new EmpWageBuilderArray();
+            empWage.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWage.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWage.Uc_10_EmpWageForMultiCompanyUsingInterface();
+        }
+    }
+}
