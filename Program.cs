d051db55904de8ed4df4488580cc8ed6a50ac71c
harshadalohar123 <harshalohar123@gmail.com>
2022-03-17 13:09:17 +0530

    ADDED: Employee Wage For Multiple Company Using Class

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmpWageForMultipleCompany
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage empWage = new EmpWage();
+            empWage.Uc_8_EmpWageForMultipleCompany("DMart", 20, 2, 10);
+            empWage.Uc_8_EmpWageForMultipleCompany("Reliance", 10, 4, 20);
+        }
+    }
+}
